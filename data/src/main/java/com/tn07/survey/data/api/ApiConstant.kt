907e920e644194ff5683476c4f95787c9b30b185
tngcanh07 <tngcanh07@gmail.com>
2021-07-15 00:05:43 +0700

    feat(oauth): implement di

--- data/src/main/java/com/tn07/survey/data/api/ApiConstant.kt
+++ data/src/main/java/com/tn07/survey/data/api/ApiConstant.kt
@@ -7,3 +7,7 @@ package com.tn07.survey.data.api
 
 internal const val OAUTH_GRANT_TYPE_PASSWORD = "password"
 internal const val OAUTH_GRANT_TYPE_REFRESH = "refresh_token"
+
+internal const val NETWORK_READ_TIME_OUT = 20L
+internal const val NETWORK_WRITE_TIME_OUT = 20L
+internal const val NETWORK_CONNECT_TIME_OUT = 20L
---
+++ data/src/main/java/com/tn07/survey/data/di/qualifier/OAuthQualifier.kt
@@ -0,0 +1,7 @@
+package com.tn07.survey.data.di.qualifier
+
+/**
+ * Created by toannguyen
+ * Jul 14, 2021 at 23:56
+ */
+annotation class OAuthQualifier()
