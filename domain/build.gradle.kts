4c773c0e1cea580a44aa5f98ba7e0f7660379c94
tngcanh07 <tngcanh07@gmail.com>
2021-07-21 18:48:59 +0700

    test: integrate jacoco into domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java-library")
     id("kotlin")
+    jacoco
 }
 
 java {
@@ -25,4 +26,28 @@ dependencies {
 
     testImplementation(Libs.JUNIT)
     testImplementation(Libs.MOCKITO)
+}
+
+tasks.jacocoTestReport {
+    classDirectories.setFrom(fileTree("$buildDir/classes") {
+        setExcludes(
+            listOf("**/test/**")
+        )
+    })
+    sourceDirectories.setFrom("${project.projectDir}/src/main/java")
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+                includes = listOf("com.tn07.survey.domain.*")
+            }
+        }
+    }
 }
