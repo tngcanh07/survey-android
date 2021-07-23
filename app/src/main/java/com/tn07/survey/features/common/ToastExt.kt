d007f4d31d6354e41985e4b584bc302b94150912
tngcanh07 <tngcanh07@gmail.com>
2021-07-24 00:54:50 +0700

    refactor: #31 handle viewbinding in base fragment

--- app/src/main/java/com/tn07/survey/features/common/ToastExt.kt
+++ app/src/main/java/com/tn07/survey/features/common/ToastExt.kt
@@ -1,12 +1,13 @@
 package com.tn07.survey.features.common
 
 import android.widget.Toast
+import androidx.viewbinding.ViewBinding
 import com.tn07.survey.features.base.BaseFragment
 
 /**
  * Created by toannguyen
  * Jul 18, 2021 at 11:03
  */
-fun BaseFragment.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+fun <T : ViewBinding> BaseFragment<T>.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(requireContext(), message, length).show()
 }
